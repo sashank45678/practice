09fbaf0ebf327fd8dfd56262321c089b55930449
sashanktadimeti <sashanktadimeti4@gmail.com>
2024-08-25 19:41:38 +0530

    added theme feature with contextapi

--- namastefood.js
+++ namastefood.js
@@ -12,17 +12,25 @@ import { Provider } from "react-redux";
 import appStore from "./src/components/appstore";
 import Cart from "./src/components/cartpage";
 import userdata from "./src/components/usercontext";
+import User from "./src/components/user";
 const Grocery = lazy(() => {
   return import("./src/grocery1");
 });
 const AppLayout = () => {
   const [username, setusername] = useState("default user..");
+  const colors=["blue","red","pink"]
+  const [color,setcolor]=useState("")
+  const changetheme=()=>{
+    const val=Math.floor(Math.random()*3)
+    setcolor(colors[val])
+  }
   useEffect(() => {
     setusername("Sashank Tadimeti");
+    setcolor("blue")
   }, []);
   return (
     <Provider store={appStore}>
-      <userdata.Provider value={{loggedin:username,setusername:setusername}}>
+      <userdata.Provider value={{loggedin:username,setusername:setusername,color:color,changetheme:changetheme}}>
         <div className="app">
           <Header />
           <Outlet />
@@ -41,13 +49,14 @@ const appRouter = createBrowserRouter([
       { path: "/about", element: <About /> },
       { path: "/contact", element: <Contact /> },
       { path: "/restaurants/:resId", element: <Restaurantmenu /> },
+      { path: "/user", element: <User /> },
       {
         path: "/grocery",
         element: (
           <Suspense fallback={<h1>loading...</h1>}>
             <Grocery />
           </Suspense>
-        ),
+        )
       },
       { path: "/cart", element: <Cart /> },
     ],
--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -3,9 +3,10 @@ import {useContext} from "react"
 import userdata from './usercontext';
 const About = () => {
     const userinfo=useContext(userdata)
-    console.log(userinfo)
+    const {color}=userinfo
+    const {changetheme}=userinfo
     return (
-        <div className="flex flex-col items-center justify-center border border-black rounded-lg my-4 px-3 py-3">
+        <div className="flex flex-col items-center justify-center border border-black rounded-lg my-4 px-3 py-3" style={{backgroundColor:color}}>
             <h1 className="text-3xl font-bold mb-4">About Our Food Delivery App</h1>
             <p className="text-lg mb-6">
                 Our food delivery app connects you with your favorite restaurants, allowing you to order delicious meals 
@@ -13,6 +14,7 @@ const About = () => {
                 With easy ordering, fast delivery, and a wide selection of cuisines, satisfaction is just a few clicks away!
             </p>
             <h1 className='font-bold text-lg'>{userinfo.loggedin}</h1>
+            <button className='px-2 py-2 rounded-md bg-black text-white' onClick={()=>{changetheme()}}>changeTheme</button>
         </div>
     );
 };
